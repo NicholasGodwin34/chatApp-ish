e49249882b0d00e933d77057e95a7cc669ca68a5
NicholasGodwin34 <128482435+NicholasGodwin34@users.noreply.github.com>
2024-11-17 19:27:02 +0300

    first version of the clientapp

--- theclient.py
+++ theclient.py
@@ -1,63 +1,58 @@
-import socket
+import socket as sc
+import threading as tr 
+import tkinter as tk 
 
-def send_request():
-    # Create a TCP socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # the AF_INET is used to specify the address family (ipv4) telling the socket to use ipv4 addresses 
-    # SOCK_STREAM specifies the socket type (tcp)
-    # if one is to make a UDP socket they would use SOCK_DGRAM
-    # if one is to make a socket that uses ipv6 addresses they'd use AF_INET6
 
-    # Connect to the server
-    host = "127.0.0.1"  # Localhost
-    port = 8080         # Same port as the server
-    client_socket.connect((host, port))
-   
-    
-    
-    # Build and send an HTTP GET request
-    request = "GET / HTTP/1.1\r\nHost: 127.0.0.1\r\nConnection: close\r\n\r\n"
-    client_socket.sendall(request.encode())
-    
-    # Receive and print the response
-    response = b""
-    while True:
-        chunk = client_socket.recv(102400)
-        if not chunk:
-            break
-        response += chunk
-    
-    print("Response:")
-    print(response.decode())
-    
-    # Close the connection
-    client_socket.close()
-
-def send_post_request():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(("127.0.0.1", 8080))
-
-    # Send a POST request with some data in the body
-    post_data = "name=JohnDoe&age=25"
-    request = f"POST / HTTP/1.1\r\nHost: 127.0.0.1\r\nContent-Length: {len(post_data)}\r\nConnection: close\r\n\r\n{post_data}"
-
-    client_socket.sendall(request.encode())
-
-    # Receive the response from the server
-    response = b""
-    while True:
-        chunk = client_socket.recv(1024)
-        if not chunk:
-            break
-        response += chunk
-
-    print("Response:")
-    print(response.decode())
-
-    client_socket.close()
+HOST = '127.0.0.1'
+PORT = 12897
+# setting up the client
+c_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+c_socket.connect((HOST , PORT))
 
 
 
-#send_post_request()
+# Function to  receive messages from the server
+def receive_msg():
+    while True : 
+        try: 
+            msg = c_socket.recv(1024).decode('utf-8')
+            if msg: 
+                chat_box.config(state='normal') # allow editing temporarily
+                chat_box.insert(tk.END, msg + '\n') # add message at the end 
+                chat_box.config(state='disabled') # disable editing again 
+                chat_box.see(tk.END) # Automatically scroll to the newest message
+        except: 
+            print("Connection to server lost.")
+            break 
 
-# Send a request
-send_request()
+# Function to send messages to the server 
+def send_msg(event=None): 
+    msg=msg_entry.get()
+    if msg: 
+        c_socket.send(msg.encode('utf-8')) # send to server
+        chat_box.config(state='normal')
+        chat_box.insert(tk.END, "You: " + msg + '\n') # display  in chat box 
+        chat_box.config(state='disabled')
+        chat_box.see(tk.END)
+        msg_entry.delete(0, tk.END) # clear the input field 
+
+# Create the GUI 
+root = tk.Tk()
+root.title("Chat Application")
+
+chat_box = tk.Text(root, height=20, width=50)
+chat_box.pack()
+
+msg_entry = tk.Entry(root, width=50)
+msg_entry.pack()
+
+send_button = tk.Button(root, text="send", command=send_msg)
+send_button.pack()
+
+#start thread to receive messages 
+root.bind("<Return>", send_msg)
+
+# Run the tkinter event loop 
+root.mainloop()
+
+
